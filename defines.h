0bd611998d9813664c87cbe7a81e5fdcf317d179
brunoherbelin <herbelin@Brunos-MacBook-Pro.local>
2020-07-31 00:15:22 +0200

    increase to v0.2

--- defines.h
+++ defines.h
@@ -5,7 +5,7 @@
 #define APP_TITLE " -- Video Live Mixer"
 #define APP_SETTINGS "vimix.xml"
 #define APP_VERSION_MAJOR 0
-#define APP_VERSION_MINOR 1
+#define APP_VERSION_MINOR 2
 #define XML_VERSION_MAJOR 0
 #define XML_VERSION_MINOR 1
 #define MAX_RECENT_HISTORY 20
