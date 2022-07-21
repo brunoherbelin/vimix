3c1f37e5f9b923446c2b20c171ab66b8964abf68
Bruno Herbelin <bruno.herbelin@gmail.com>
2022-07-21 22:42:32 +0200

    BugFix GLSL shader array init

--- rsc/shaders/filters/bilinear.glsl
+++ rsc/shaders/filters/bilinear.glsl
@@ -1,20 +1,18 @@
-// Fast simple bilinear RGBA filter
+// Fast simple bilateral RGBA filter
 // by Bruno Herbelin
-
-#define SIGMA 10.0
 
 uniform float Factor;
 
-float kernel[15] = {0.031225216, 0.033322271, 0.035206333, 0.036826804, 0.038138565,
+float kernel[15] = float[15](0.031225216, 0.033322271, 0.035206333, 0.036826804, 0.038138565,
 0.039104044, 0.039695028, 0.039894000, 0.039695028, 0.039104044,
-0.038138565, 0.036826804, 0.035206333, 0.033322271, 0.031225216 };
+0.038138565, 0.036826804, 0.035206333, 0.033322271, 0.031225216);
 
 float normpdf(in float x, in float sigma)
 {
     return 0.39894*exp(-0.5*x*x/(sigma*sigma))/sigma;
 }
 
-float normpdf3(in vec4 v, in float sigma)
+float normpdf4(in vec4 v, in float sigma)
 {
     return 0.39894*exp(-0.5*dot(v,v)/(sigma*sigma))/sigma;
 }
@@ -25,7 +23,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     float Z = 0.0;
     vec4 cc;
     float bsigma = mix(0.01, 0.5, Factor);
-    float bZ = 1.0/normpdf(0.0, mix(bsigma, 1.0, .0) );
+    float bZ = 1.0/normpdf(0.0, mix(bsigma, 1.0, 0.0) );
     vec4 C = texture(iChannel0, (fragCoord.xy / iResolution.xy));
 
     for (int i = -7; i <= 7; ++i)
@@ -33,7 +31,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
         for (int j = -7; j <= 7; ++j)
         {
             cc = texture(iChannel0, (fragCoord+vec2(float(i),float(j))) / iResolution.xy);
-            float f = normpdf3(cc -C, mix(bsigma, 1.0, .0) ) * bZ * kernel[7+j] * kernel[7+i];
+            float f = normpdf4(cc -C, mix(bsigma, 1.0, 0.0) ) * bZ * kernel[7+j] * kernel[7+i];
             Z += f;
             O += f*cc;
         }
