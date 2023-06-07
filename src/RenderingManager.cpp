brunoherbelin/vimix

commit c0befa0f494d1d36805d2ff3a71fd092a59815b0

Show vimix main window on attempt to close an output window
Because only vimix Displays view is allowed to manage output windows, it is not possible to close the window from system window manager. Instead, we show vimix and the Displays view to help the user.

src/RenderingManager.cpp
@@ -178,8 +178,14 @@ static void OutputWindowEvent( GLFWwindow *w, int button, int action, int)
 
 static void WindowCloseCallback( GLFWwindow* w )
 {
-    if (!UserInterface::manager().TryClose())
-        glfwSetWindowShouldClose(w, GLFW_FALSE);
+    if (Rendering::manager().mainWindow().window() == w) {
+        if (!UserInterface::manager().TryClose())
+            glfwSetWindowShouldClose(w, GLFW_FALSE);
+    }
+    else {
+        Mixer::manager().setView(View::DISPLAYS);
+        Rendering::manager().mainWindow().show();
+    }
 }
 
 void Rendering::MonitorConnect(GLFWmonitor* monitor, int event)
@@ -950,6 +956,7 @@ bool RenderingWindow::init(int index, GLFWwindow *share)
     //
     // all windows capture keys
     glfwSetKeyCallback( window_, Control::keyboardCalback);
+    glfwSetWindowCloseCallback( window_, WindowCloseCallback );
 
     if (master_ != NULL) {
         // additional window callbacks for user input in output windows
@@ -957,7 +964,6 @@ bool RenderingWindow::init(int index, GLFWwindow *share)
     }
     else {
         // additional window callbacks for main window
-        glfwSetWindowCloseCallback( window_, WindowCloseCallback );
         glfwSetDropCallback( window_, Rendering::FileDropped);
     }
 

commit 3251e9f845f9375a7a03cff5d5842712c9e71394

Fix Show vimix main window on output close
Commit c0befa0f494d1d36805d2ff3a71fd092a59815b0 introduced a bug under OSX. Now fixed.

src/RenderingManager.cpp
@@ -182,7 +182,7 @@ static void WindowCloseCallback( GLFWwindow* w )
         if (!UserInterface::manager().TryClose())
             glfwSetWindowShouldClose(w, GLFW_FALSE);
     }
-    else {
+    else if (!glfwWindowShouldClose(w)) {
         Mixer::manager().setView(View::DISPLAYS);
         Rendering::manager().mainWindow().show();
     }
@@ -425,7 +425,7 @@ void Rendering::show()
 
 bool Rendering::isActive()
 {
-    return !glfwWindowShouldClose(main_.window());
+    return ( main_.window() != NULL && !glfwWindowShouldClose(main_.window()) );
 }
 
 void Rendering::pushBackDrawCallback(RenderingCallback function)