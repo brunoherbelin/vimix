0e110720709beddb9918431f1324b4bef0737f38
brunoherbelin <bruno.herbelin@gmail.com>
2025-10-30 23:28:00 +0100

    BugFix Set media player speed on session load and Rewind (allows start with negative playspeed)

--- src/SessionCreator.cpp
+++ src/SessionCreator.cpp
@@ -1156,10 +1156,13 @@ void SessionLoader::visit (MediaSource& s)
     // set config media player
     s.mediaplayer()->accept(*this);
 
-    // add a callback to activate play speed
+    // read source play speed from media player SessionLoader::visit
     double r = s.mediaplayer()->playSpeed();
-    s.mediaplayer()->setRate(1.0); // reset to normal speed
-    s.call( new PlaySpeed( r, 100 ) );
+    // reset media player to normal speed
+    s.mediaplayer()->setRate(1.0); 
+    // add a callback to activate source play speed and rewind
+    s.call( new PlaySpeed( r ) );
+    s.call( new RePlay( ) );
 }
 
 void SessionLoader::visit (SessionFileSource& s)
