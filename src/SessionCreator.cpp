066158b5dc09ce368ae936928e5bf4ba42c42d2f
brunoherbelin <bruno.herbelin@gmail.com>
2026-01-01 09:06:55 +0100

    BugFix (introduced in 1944231ed6d6dddba75495a46356e81ffd3cc99b)

--- src/SessionCreator.cpp
+++ src/SessionCreator.cpp
@@ -1104,12 +1104,12 @@ void SessionLoader::visit (Source& s)
         // set id of source used as mask (if exists)
         uint64_t id__ = 0;
         xmlCurrent_->QueryUnsigned64Attribute("source", &id__);
-        if (id__ > 0) {
+        if (id__ > 0) 
             s.maskSource()->connect(id__, session_);
-            s.touch(Source::SourceUpdate_Mask);
-        }
         // set the mask from jpeg (if exists)
         s.setMask( SessionLoader::XMLToImage(xmlCurrent_) );
+        // update mask
+        s.touch(Source::SourceUpdate_Mask);
     }
 
     xmlCurrent_ = sourceNode->FirstChildElement("ImageProcessing");
