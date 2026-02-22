brunoherbelin/vimix

commit 02f70735e05a760c8c549bb0b0acabe61cdb2aeb

Fix render initialization check to include texture index validation

src/Source/MediaSource.cpp
@@ -214,7 +214,8 @@ void MediaSource::updateAudio()
 
 void MediaSource::render()
 {
-    if ( renderbuffer_ == nullptr )
+    if ( renderbuffer_ == nullptr || 
+         texturesurface_->textureIndex() != mediaplayer_->texture() )
         init();
     else {
         // render the media player into frame buffer

commit 86ff269bd67839879556f8a7d739e20ece806b8d

Bugfix Revert commit 02f70735e05a760c8c549bb0b0acabe61cdb2aeb which caused unnecessary reset of media source

src/Source/MediaSource.cpp
@@ -219,8 +219,7 @@ void MediaSource::updateAudio()
 
 void MediaSource::render()
 {
-    if ( renderbuffer_ == nullptr || 
-         texturesurface_->textureIndex() != mediaplayer_->texture() )
+    if ( renderbuffer_ == nullptr )
         init();
     else {
         // render the media player into frame buffer